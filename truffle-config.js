ee8b9994296ab8c86e2f8e1c87683144207d4fbd
imef-femi <bolajifemi28@gmail.com>
2021-07-26 01:14:31 +0100

    --- Auto-Git Commit ---

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,7 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const sokol = 'https://sokol.poa.network';
 const kccTestNetwork = 'https://rpc-testnet.kcc.network';
+const kccMainNetwork = 'https://rpc-mainnet.kcc.network';
 const bscTest = "https://data-seed-prebsc-2-s3.binance.org:8545/";
 
 const fs = require("fs");
@@ -19,7 +20,11 @@ module.exports = {
       network_id: 77, // This network is yours, in the cloud.
       production: true, // Treats this network as if it was a public net. (default: false)
     },
-
+  kccMainNet: {
+      provider: () => new HDWalletProvider(mnemonic, kccMainNetwork),
+      network_id: 323, // This network is yours, in the cloud.
+      production: true, // Treats this network as if it was a public net. (default: false)
+    },
     kccTestNet: {
       provider: () => new HDWalletProvider(mnemonic, kccTestNetwork),
       network_id: 322, // This network is yours, in the cloud.
